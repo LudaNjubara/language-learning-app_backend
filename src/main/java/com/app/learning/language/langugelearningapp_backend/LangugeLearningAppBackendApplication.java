15947e00155bb6b7430a591b98f66357c38cd59a
LudaNjubara <mihael.sestak00@gmail.com>
2023-10-09 18:56:27 +0200

    Initial commit

---
+++ src/main/java/com/app/learning/language/langugelearningapp_backend/LangugeLearningAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.learning.language.langugelearningapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangugeLearningAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangugeLearningAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/learning/language/langugelearningapp_backend/LangugeLearningAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.learning.language.langugelearningapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangugeLearningAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
